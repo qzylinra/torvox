29f895bd25280ceab848d398237d3174a70e4a9f
jane <jane@computer.local>
2026-05-27 08:24:35 +0000

    feat(P0.1): torvox-fuzz, torvox-integration-tests, torvox-bench

---
+++ torvox-fuzz/src/lib.rs
@@ -0,0 +1,11 @@
+pub fn fuzz_vt_parser(data: &[u8]) {
+    let _ = data;
+}
+
+pub fn fuzz_osc_parse(data: &[u8]) {
+    let _ = data;
+}
+
+pub fn fuzz_grid_resize(data: &[u8]) {
+    let _ = data;
+}
